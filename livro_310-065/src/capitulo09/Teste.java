bc45e9e10e5cae9bc7a12c851b6366b51e2a18be
Josias Fabrício <contato@josiasfabricio.com.br>
2015-03-19 15:38:24 -0300

    pacote do capitulo 09 sobre Threads

---
+++ livro_310-065/src/capitulo09/Teste.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package capitulo09;
+
+/**
+ * @author jfsilva
+ * 19/03/2015
+ */
+public class Teste implements Runnable {
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @author jfsilva 
+	 * @version last update: 19/03/2015
+	 * @param args 
+	 * void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
