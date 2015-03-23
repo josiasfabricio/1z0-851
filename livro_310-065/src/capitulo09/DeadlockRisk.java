0debb66c74f08952a4d730ab07cff57f3be6243e
Josias Fabrício <contato@josiasfabricio.com.br>
2015-03-23 20:11:08 -0300

    implementação de um deadlock

--- livro_310-065/src/capitulo09/DeadlockRisk.java
+++ livro_310-065/src/capitulo09/DeadlockRisk.java
@@ -7,8 +7,8 @@ package capitulo09;
 /**
  * @author Josias 22/03/2015
  */
-public class DeadlockRisk {
-	
+public class DeadlockRisk implements Runnable {
+
 	private static class Resource {
 		public int value;
 	}
@@ -16,23 +16,45 @@ public class DeadlockRisk {
 	private Resource resourceA = new Resource();
 	private Resource resourceB = new Resource();
 
-	public int read() {
-		synchronized (resourceA) { // May deadlock here
+	public void read() throws InterruptedException {
+		synchronized (resourceA) { // May deadlock here			
 			synchronized (resourceB) {
-				return resourceB.value + resourceA.value;
+				System.out.printf("%s -> A: %d B: %d %n", Thread.currentThread()
+						.getName(), resourceA.value, resourceB.value);				
 			}
+
 		}
 	}
 
-	public void write(int a, int b) {
-		synchronized (resourceB) { // May deadlock here
+	public void write(int a, int b) throws InterruptedException {
+		synchronized (resourceB) { // May deadlock here			
 			synchronized (resourceA) {
 				resourceA.value = a;
-				resourceB.value = b;
+				resourceB.value = b;				
 			}
 		}
 	}
-	
-	// TODO public static void main(string[] args){}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		try {
+			this.write(1, 2);
+			this.read();
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String... args) {
+		Thread t1 = new Thread(new DeadlockRisk(), "DeadLock 1");
+		t1.start();
+		Thread t2 = new Thread(new DeadlockRisk(), "DeadLock 2");
+		t2.start();
+	}
 }
